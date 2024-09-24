57bdb31e2ee935e3cb125d1847bacf236543339c
Olivia Stewart <liv.stewart2004@gmail.com>
2024-09-24 13:47:02 +1000

    Add project files.

---
+++ CodeGenLecture/CodeSource.cs
@@ -0,0 +1,16 @@
+﻿namespace  CodeGenLecture;
+public static class CodeSource
+{
+	public static string HelloWorld => """
+	                             using System;
+	                             using System.IO;
+
+	                             public static class Program
+	                             {
+	                             	public static void Main()
+	                             	{
+	                             		Console.WriteLine("Hello, World!");
+	                             	}
+	                             }
+	                             """;
+}
